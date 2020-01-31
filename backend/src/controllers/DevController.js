7c0392bb41e3dbf1bebd2f8ee42797d238330df0
Pliavi <pliavi.rpgm@gmail.com>
2020-01-31 17:00:14 -0300

    Remove parseStringAsArray in store

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,47 +1,57 @@
-const axios = require('axios')
-const Dev = require('../models/Dev')
-const parseStringAsArray = require('../utils/parseStringAsArray')
-const { findConnections, sendMessage } = require('../websocket')
+const axios = require("axios");
+const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
-  async index(request, response){
-    const devs = await Dev.find()
+  async index(request, response) {
+    const devs = await Dev.find();
 
-    return response.json(devs)
+    return response.json(devs);
   },
 
   async store(request, response) {
-    const { github_username, techs, latitude, longitude, contact_accounts } = request.body
+    const {
+      github_username,
+      latitude,
+      longitude,
+      contact_accounts
+    } = request.body;
 
-    let dev = await Dev.findOne({ github_username })
+    const techs = request.body.techs.map(tech => tech.toUpperCase);
+
+    let dev = await Dev.findOne({ github_username });
 
     if (!dev) {
-      const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-      const { name = login, avatar_url, bio } = apiResponse.data
-
-      const techsArray = parseStringAsArray(techs)
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
       const location = {
         type: "Point",
         coordinates: [longitude, latitude]
-      }
+      };
 
       dev = await Dev.create({
         github_username,
         name,
         avatar_url,
         bio,
-        techs: techsArray,
+        techs,
         contact_accounts,
         location
-      })
+      });
 
-      const sendSocketMessageTo = findConnections({latitude, longitude}, techsArray) 
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techs
+      );
 
-      sendMessage(sendSocketMessageTo, 'new-dev', dev)
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     }
 
-    return response.json(dev)
+    return response.json(dev);
   }
-}
+};
